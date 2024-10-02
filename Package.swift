c848b012c791e683beb10f9c30dd6b92b6987fe1
Raymond Jones <raymond.jones@kustomer.com>
2024-10-02 13:14:07 -0400

    bump version to 5.0.2

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 
 let kustomerChatFramework = Target.binaryTarget(
   name: "KustomerChatSDK",
-  url: "https://github.com/kustomer/kustomer-ios/archive/refs/tags/5.0.1.zip",
-  checksum: "855c8ddcea1a38fc3b3c5a982f02fa7bc694e4f56513e1efc0f01030d52d435d")
+  url: "https://github.com/kustomer/kustomer-ios/archive/refs/tags/5.0.2.zip",
+  checksum: "c6763af0058cc220b9cf32c242d1a7cd46d9e2114d1445beeef7718e1e245ee9")
 
 let package = Package(
   name: "kustomer-ios",
